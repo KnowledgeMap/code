05332dfc123401c726127b1e2079d9548139cb9c
Jackon Yang <jiekunyang@gmail.com>
2014-11-12 14:00:55 +0800

    show accounts name in home page after login

--- kmap/views.py
+++ kmap/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render_to_response
+from django.template import RequestContext
 
 def home(request):
-    return render_to_response('home.html')
+    content = dict()
+    return render_to_response('home.html', RequestContext(request, content))
