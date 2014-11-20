a2587cf1f30d2b2621a0d7d6ad2ef40230b63bb0
Jackon Yang <jiekunyang@gmail.com>
2014-11-20 20:00:38 +0800

    get net data from database

--- kmap/views.py
+++ kmap/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.http import HttpResponse
+from kmap.models import kGraph
 
 import json
 
@@ -10,20 +11,22 @@ def home(request):
     return render_to_response('home.html', RequestContext(request, content))
 
 def get_net(request):
-    response_data = {}
-    response_data['nodes'] = [
-            {"id":"广义相对论"},
-            {"id":"狭义相对论"},
-            {"id":"电动力学"},
-            {"id":"非相对论量子力学"},
-            {"id":"固体物理"},
-            {"id":"量子场论"}
-            ]
+    p = kGraph.objects.all()
+    nodes = []
+    edges = []
+    for item in p:
+        nodes.extend([
+            {"id": '%s-%s' % (item.id, item.edge.name)},
+            {"id": item.prop1.name},
+            {"id": item.prop2.name},
+            ])
+        edges.extend([
+            {"source": '%s-%s' % (item.id, item.edge.name),"target": item.prop1.name},
+            {"source": '%s-%s' % (item.id, item.edge.name),"target": item.prop2.name},
+            ])
+        if item.prop3:
+            nodes.append({"id": item.prop3.name})
+            edges.append({"source": '%s-%s' % (item.id, item.edge.name),"target": item.prop3.name})
 
-    response_data['edges'] = [
-            {"source":"狭义相对论","target":"量子场论"},
-            {"source":"广义相对论","target":"狭义相对论"},
-            {"source":"固体物理","target":"量子场论"}
-            ]
-
+    response_data = {'nodes': nodes, 'edges': edges}
     return HttpResponse(json.dumps(response_data), content_type="application/json")
