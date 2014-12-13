512a0164e5e33952fd6e254b160e8692ca34ccf4
renminghao <332020883@qq.com>
2014-12-13 16:54:29 +0800

    edit page

--- urls.py
+++ urls.py
@@ -8,5 +8,6 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^kmap/', include('kmap.urls')),
-    url(r'^accounts/', include('accounts.urls'))
+    url(r'^accounts/', include('accounts.urls')),
+    url(r'^edit/', 'kmap.views.edit', name = 'home'),
 )
