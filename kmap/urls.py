1956c077beb169370c087b520db7dd9002f7a791
Jackon Yang <jiekunyang@gmail.com>
2014-12-13 17:25:50 +0800

    edit.html template

--- kmap/urls.py
+++ kmap/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
+
+    url(r'^edit/$', 'kmap.views.edit', name = 'edit'),
+
     url(r'^net/$', 'kmap.views.get_net', name='net'),  # current default
     url(r'^net1/$', 'kmap.views.get_net1', name='net1'),
     url(r'^net2/$', 'kmap.views.get_net2', name='net2'),
--- urls.py
+++ urls.py
@@ -9,5 +9,4 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^kmap/', include('kmap.urls')),
     url(r'^accounts/', include('accounts.urls')),
-    url(r'^edit/', 'kmap.views.edit', name = 'home'),
 )
