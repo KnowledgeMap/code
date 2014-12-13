1c3b64892b1cfede92beddfbbac88bcb50afa557
renminghao <332020883@qq.com>
2014-12-13 16:47:35 +0800

    整合edit页面

--- kmap/views.py
+++ kmap/views.py
@@ -9,7 +9,9 @@ import json
 def home(request):
     content = dict()
     return render_to_response('home.html', RequestContext(request, content))
-
+def edit(request):
+	content = dict()
+	return render_to_response('edit.html', RequestContext(request, content))
 
 def get_algorithm(request):
     response_data = [
